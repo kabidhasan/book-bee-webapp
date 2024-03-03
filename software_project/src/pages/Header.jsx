dac46c8b414539b9faf6ce6fa15ce0bd4a419a70
imehedi357 <imehedi357@gmail.com>
2024-03-04 02:40:27 +0600

    file system issue fix

--- software_project/src/pages/Header.jsx
+++ software_project/src/pages/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Button from "../../components/ui/Button";
+import Button from "/src/components/ui/Button";
 function Header() {
 	return (
 		<div className='flex justify-between items-center bg-gradient-to-t from-pri to-teal-800 px-24 pt-4 h-full'>
--- software_project/src/pages/MainSection.jsx
+++ software_project/src/pages/MainSection.jsx
@@ -1,4 +1,4 @@
-import Button from "../../components/ui/Button";
+import Button from "/src/components/ui/Button";
 function MainSection() {
 	return (
 		<>
