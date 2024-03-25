af77b91b1471bd8ee22534dab5c2d8846428a400
Kabid Hasan <kabidhasan34@gmail.com>
2024-03-26 00:48:41 +0600

    request list in benificiary side dynamically implemented

---
+++ software_project/src/components/ui/RequestRows.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const RequestRows = ({ request }) => {
+    const { bookId, benificiary } = request
+    console.log(benificiary)
+    const [benificiaryObj, setBenificiaryObj] = useState({})
+    const [book, setBook] = useState({})
+    const fetchData = async () => {
+        axios.get(`http://localhost:3000/user/getUserByUserId/${benificiary}`).then(res => setBenificiaryObj(res.data))
+        axios.get(`http://localhost:3000/book/getBookById/${bookId}`).then(res=>setBook(res.data.book))
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+    return (
+      <tr>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src={book.image} alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+                        <div className="font-bold">{benificiaryObj.name}</div>
+              <div className="text-sm opacity-50">United States</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          {book.name}
+          <br />
+                <span className="badge badge-ghost badge-sm">{book.author}</span>
+        </td>
+        <th className="flex h-20 justify-center items-center gap-6">
+          <button className="btn btn-success btn-xs">RESPOND</button>
+          <button className="btn btn-error btn-xs ">IGNORE</button>
+        </th>
+      </tr>
+    );
+};
+
+export default RequestRows;
--- software_project/src/components/ui/Requests.jsx
+++ software_project/src/components/ui/Requests.jsx
@@ -1,100 +1,40 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import RequestRows from "./RequestRows"
 function Requests() {
-    return (
-
-        <div className="overflow-x-auto w-full border border-black rounded-sm ">
-            <table className="table text-white text-lg">
-                {/* head */}
-                <thead className='text-orange-400  text-xl text-center'>
-                    <tr>
-                        <th>User Name</th>
-                        <th>Book</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* row 1 */}
-                    <tr>
-                        <td>
-                            <div className="flex items-center gap-3">
-                                <div className="avatar">
-                                    <div className="mask mask-squircle w-12 h-12">
-                                        <img src="/book.png" alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="font-bold">Hart Hagerty</div>
-                                    <div className="text-sm opacity-50">United States</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Harry Potter
-                            <br />
-                            <span className="badge badge-ghost badge-sm">J.K. Rowling</span>
-                        </td>
-                        <th className='flex h-20 justify-center items-center gap-6'>
-                            <button className="btn btn-success btn-xs">ACCEPT</button>
-                            <button className="btn btn-error btn-xs ">REJECT</button>
-                        </th>
-                    </tr>
-                    {/* row 2 */}
-                    <tr>
-                        <td>
-                            <div className="flex items-center gap-3">
-                                <div className="avatar">
-                                    <div className="mask mask-squircle w-12 h-12">
-                                        <img src="/book.png" alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="font-bold">Brice Swyre</div>
-                                    <div className="text-sm opacity-50">China</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Dune
-                            <br />
-                            <span className="badge badge-ghost badge-sm">Frank Herbert</span>
-                        </td>
-                        <th className='flex h-20 justify-center items-center gap-6'>
-                            <button className="btn btn-success btn-xs">ACCEPT</button>
-                            <button className="btn btn-error btn-xs ">REJECT</button>
-                        </th>
-                    </tr>
-                    {/* row 3 */}
-                    <tr>
-                        <td>
-                            <div className="flex items-center gap-3">
-                                <div className="avatar">
-                                    <div className="mask mask-squircle w-12 h-12">
-                                        <img src="/book.png" alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="font-bold">Marjy Ferencz</div>
-                                    <div className="text-sm opacity-50">Russia</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Physics
-                            <br />
-                            <span className="badge badge-ghost badge-sm">Jafor Iqbal</span>
-                        </td>
-                        <th className='flex h-20 justify-center items-center gap-6'>
-                            <button className="btn btn-success btn-xs">ACCEPT</button>
-                            <button className="btn btn-error btn-xs ">REJECT</button>
-                        </th>
-                    </tr>
-                    </tbody>
-
-            </table>
-        </div>
-
-    )
+  const [requests, setRequests] = useState([]);
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:3000/book/getAllRequestByContributorId?contributorId=${localStorage.getItem(
+        "userId"
+      )}`
+      );
+      await setRequests(res.data.requests)
+      
+  };
+  useEffect(() => {fetchData()}, []);
+  return (
+    <div className="overflow-x-auto w-full border border-black rounded-sm ">
+      <table className="table text-white text-lg">
+        {/* head */}
+        <thead className="text-orange-400  text-xl text-center">
+          <tr>
+            <th>User Name</th>
+            <th>Book</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+              <tbody>
+                  {requests.map(request => {
+                      
+                      return ( <RequestRows key={request._id} request={request}></RequestRows> );
+                  })}
+          
+          
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Requests
+export default Requests;
