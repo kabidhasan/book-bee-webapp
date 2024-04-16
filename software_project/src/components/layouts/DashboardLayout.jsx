00ceb3b2bc79dd739068037cbc1ffc04cba37492
Kabid Hasan <kabidhasan34@gmail.com>
2024-04-16 20:48:56 +0600

    implemented dynamic name, email of logged in user. changed the default avatar photo.

--- software_project/src/components/layouts/DashboardLayout.jsx
+++ software_project/src/components/layouts/DashboardLayout.jsx
@@ -5,41 +5,57 @@ import Navbar from "../ui/Navbar";
 import { Outlet } from "react-router-dom";
 import TopContributors from "../ui/TopContributors";
 import PermissionDenied from "../../pages/PermissionDenied";
-
-const user = {
-	name: "Adnan Chowdhury",
-	email: "helloworld@gmail.com",
-	imageUrl:
-		"https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
-	bio: "I am a software developer. I love tutorial books.",
-	location: "Akhalia, Sylhet, Bangladesh",
-	joined: "March 2021",
-	points: 100,
-};
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function DashboardLayout() {
-	if (!localStorage.getItem("userId")) {
-		console.log();
-		return <PermissionDenied />;
-	}
-	return (
-		<>
-			<div className="container mx-auto mb-auto bg-pri">
-				<DashHeader />
-				<Navbar />
-				<main className="w-full h-auto flex divide-x-2 divide-black/50">
-					<div className="w-4/6 p-4 pl-0 flex flex-col justify-start items-center">
-						<Outlet />
-					</div>
-					<div className="w-2/6  p-4 pr-0">
-						<Profile user={user} />
-						<TopContributors />
-					</div>
-				</main>
-			</div>
-			<Footer />
-		</>
-	);
+  const [user, setUser] = useState({
+    name: "Adnan Chowdhury",
+    email: "helloworld@gmail.com",
+    imageUrl: "/avatar.jpg",
+    bio: "I am a software developer. I love tutorial books.",
+    location: "Akhalia, Sylhet, Bangladesh",
+    joined: "March 2021",
+    points: 100,
+  });
+  const fetchData = async () => {
+    if (localStorage.getItem("userId")) {
+      const res = await axios.get(
+        `http://localhost:3000/user/getUserByUserId/${localStorage.getItem(
+          "userId"
+        )}`
+      );
+      console.log(res);
+      setUser({ ...user, name: res.data.name, email: res.data.email });
+
+      console.log(user.name);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (!localStorage.getItem("userId")) {
+    console.log();
+    return <PermissionDenied />;
+  }
+  return (
+    <>
+      <div className="container mx-auto mb-auto bg-pri">
+        <DashHeader />
+        <Navbar />
+        <main className="w-full h-auto flex divide-x-2 divide-black/50">
+          <div className="w-4/6 p-4 pl-0 flex flex-col justify-start items-center">
+            <Outlet />
+          </div>
+          <div className="w-2/6  p-4 pr-0">
+            <Profile user={user} />
+            <TopContributors />
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default DashboardLayout;
